9b81d360cdc7e5d4b6472810335fef032c3c023c
lawrence-254 <lodhiambo26@gmail.com>
2024-10-10 14:46:12 +0300

    cleanup

--- src/components/work/embeded_video.jsx
+++ src/components/work/embeded_video.jsx
@@ -1,60 +1,3 @@
-// import './embeded_style.css';
-
-// const renderVideo = (vid_url) => {
-//   if (!vid_url || vid_url.length === 0) return null;
-
-//   const observeVideos = (videoElements) => {
-//     const observer = new IntersectionObserver(
-//       (entries, obs) => {
-//         entries.forEach((entry) => {
-//           if (entry.isIntersecting) {
-//             const iframe = entry.target.querySelector('iframe');
-//             if (iframe && !iframe.src) {
-//               iframe.src = iframe.getAttribute('data-src');
-//             }
-//             obs.unobserve(entry.target);
-//           }
-//         });
-//       },
-//       { rootMargin: '100px' }
-//     );
-
-//     videoElements.forEach((el) => observer.observe(el));
-//   };
-
-//   setTimeout(() => {
-//     const videoContainers = document.querySelectorAll('.video-container');
-//     observeVideos(videoContainers);
-//   }, 0);
-
-//   return vid_url.map((url, index) => {
-//     let embedUrl;
-//     if (url.includes('vimeo.com')) {
-//       const vimeoId = url.split('/').pop();
-//       embedUrl = `https://player.vimeo.com/video/${vimeoId}`;
-//     } else if (url.includes('youtube.com') || url.includes('youtu.be')) {
-//       const youtubeId = url.includes('youtube.com')
-//         ? new URLSearchParams(new URL(url).search).get('v')
-//         : url.split('/').pop();
-//       embedUrl = `https://www.youtube.com/embed/${youtubeId}`;
-//     }
-
-//     return (
-//       <div key={index} className="video-container">
-//         <iframe
-//           data-src={embedUrl}
-//           frameBorder="0"
-//           className="video-iframe"
-//           allow="autoplay; fullscreen"
-//           allowFullScreen
-//           title={`Video ${index + 1}`}
-//         ></iframe>
-//       </div>
-//     );
-//   });
-// };
-
-// export default renderVideo;
 import './embeded_style.css';
 
 const renderVideo = (vid_url) => {
@@ -84,7 +27,6 @@ const renderVideo = (vid_url) => {
     }
 
     return (
-    // <div key={index}>
     <div key={index} className="video-container">
       <div className="video-wrapper">
       <iframe
@@ -97,7 +39,6 @@ const renderVideo = (vid_url) => {
       ></iframe>
       </div>
     </div>
-  // </div>
     );
   });
 };
