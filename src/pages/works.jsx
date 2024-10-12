005f409b4a60a6d371c55140d044451b18fb6e5f
lawrence-254 <lodhiambo26@gmail.com>
2024-10-12 16:55:37 +0300

    circle

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -1209,7 +1209,7 @@ const ShowWorks = ({
                       xmlns="http://www.w3.org/2000/svg"
                       width="36"
                       height="36"
-                      viewBox="0 10 20 70"
+                      viewBox="0 0 30 70"
                       fill="none"
                       onClick={handleRemoveClass} 
                     >
