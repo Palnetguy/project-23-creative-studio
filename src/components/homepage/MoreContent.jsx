fe5d97f8646c4d350ecc6ec560bdb9700b62d5bb
lawrence-254 <lodhiambo26@gmail.com>
2024-10-18 17:55:50 +0300

    1

--- src/components/homepage/MoreContent.jsx
+++ src/components/homepage/MoreContent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../footer";
 import { teamMembers } from "./TeamData";
+import useResponsive from "../../utils/useResponsive";
 
 const TeamMember = ({ name, image, role }) => (
     <div className="mber">
@@ -14,21 +15,22 @@ const TeamMember = ({ name, image, role }) => (
   );
   
   const MoreContent = () => {
+    const screenSize= useResponsive()
     return (
-      <div className="more_content">
-        <div className="preshow"></div>
+      <div className="more_content"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}>
+        <div className="preshow"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}></div>
   
-        <h1 className="ourteam">OUR TEAM</h1>
-        <div className="our_team">
+        <h1 className="ourteam"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}>OUR TEAM</h1>
+        <div className="our_team"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}>
           {teamMembers.map((member, index) => (
             <TeamMember key={index} {...member} />
           ))}
         </div>
   
-        <div className="btns">
+        <div className="btns"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}>
           {['works', 'contact'].map((page) => (
             <Link key={page} to={`/${page}`} target="_top">
-              <div className="btn">{page.toUpperCase()}</div>
+              <div className="btn"style={{ fontSize: screenSize === 'mobile' ? '14px' : screenSize === 'largeDesktop' ? '22px' : '18px' }}>{page.toUpperCase()}</div>
             </Link>
           ))}
         </div>
